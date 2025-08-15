c200fbdcdce7fdac5e61f29ac5f7ade241e1b6c2
Iren <irunuch@gmail.com>
2025-08-15 23:30:50 +0300

    lending

---
+++ script.js
@@ -0,0 +1,34 @@
+const slider = document.querySelector('.slider');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+const scrollStep = 220;
+
+function scrollNext() {
+
+    if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth - scrollStep) {
+        slider.scrollTo({ left: 0, behavior: 'smooth' });
+    } else {
+        slider.scrollBy({ left: scrollStep, behavior: 'smooth' });
+    }
+}
+
+function scrollPrev() {
+
+    if (slider.scrollLeft <= 0) {
+        slider.scrollTo({ left: slider.scrollWidth, behavior: 'smooth' });
+    } else {
+        slider.scrollBy({ left: -scrollStep, behavior: 'smooth' });
+    }
+}
+
+nextBtn.addEventListener('click', scrollNext);
+prevBtn.addEventListener('click', scrollPrev);
+
+let autoScroll = setInterval(scrollNext, 2000);
+
+
+slider.addEventListener('mouseenter', () => clearInterval(autoScroll));
+slider.addEventListener('mouseleave', () => {
+    autoScroll = setInterval(scrollNext, 2000);
+});
